9933480b82ff3797b7f0fc368af3a696b2f38afc
Derek Chiong <dtchiong@gmail.com>
2020-04-07 20:51:42 -0700

    Add user model

---
+++ OnlineOrderPrinter/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineOrderPrinter.Models {
+    class User {
+        public string Id { get; set; }
+        public string RestaurantId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
+        public string UserType { get; set; }
+    }
+}
