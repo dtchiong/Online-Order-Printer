fd813975af0e40ab985a072d79972433b95f9188
Derek Chiong <dtchiong@gmail.com>
2020-04-08 17:20:45 -0700

    Add Event model class

---
+++ OnlineOrderPrinter/Models/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineOrderPrinter.Models {
+    class Event {
+        public string Id { get; set; }
+        public string EventType { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string RestaurantId { get; set; }
+        public Order Order { get; set; }
+    }
+}
