27d5b8656f49f20b66f3c02d5cfb5a3b7ae60e68
Derek Chiong <dtchiong@gmail.com>
2020-04-09 17:34:55 -0700

    Add AppState class to store the fetched events, and FetchEvents saga to fetch the events and store the response in AppState

---
+++ OnlineOrderPrinter/Sagas/EventSagas.cs
@@ -0,0 +1,41 @@
+﻿using OnlineOrderPrinter.Apis;
+using OnlineOrderPrinter.Apis.Responses;
+using OnlineOrderPrinter.State;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OnlineOrderPrinter.Sagas {
+    class EventSagas {
+        private static int FetchingEvents = 0;
+
+        public static void FetchEvents(
+            string restaurantId,
+            string bearerToken,
+            string startEventId = null,
+            DateTime? startTime = null,
+            DateTime? endTime = null) {
+
+            if (Interlocked.Exchange(ref FetchingEvents, 1) == 0) {
+                Debug.WriteLine("Starting FetchEvents saga");
+                Task.Run(() => {
+                    FetchEventsResponse response = Api.FetchEvents(
+                        restaurantId,
+                        bearerToken,
+                        startEventId,
+                        startTime,
+                        endTime).Result;
+                    if (response.IsSuccessStatusCode()) {
+                        AppState.ReceiveEvents(response.Events);
+                    }
+                    Debug.WriteLine("Ended FetchEvents saga");
+                    Interlocked.Exchange(ref FetchingEvents, 0);
+                });
+            }
+        }
+    }
+}
---
+++ OnlineOrderPrinter/State/AppState.cs
@@ -0,0 +1,18 @@
+﻿using OnlineOrderPrinter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineOrderPrinter.State {
+    class AppState {
+        public static List<Event> Events = new List<Event>();
+
+        public static void ReceiveEvents(Event[] events) {
+            foreach (Event @event in events) {
+                Events.Add(@event);
+            }
+        }
+    }
+}
