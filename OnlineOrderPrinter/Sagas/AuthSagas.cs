823ca3e01d370107736dd40ea4c11be3089abd48
Derek Chiong <dtchiong@gmail.com>
2020-04-09 18:32:29 -0700

    Add Authenticate saga. Add the authenticated user to AppState

---
+++ OnlineOrderPrinter/Sagas/AuthSagas.cs
@@ -0,0 +1,33 @@
+﻿using OnlineOrderPrinter.Apis;
+using OnlineOrderPrinter.Apis.Responses;
+using OnlineOrderPrinter.State;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OnlineOrderPrinter.Sagas {
+    class AuthSagas {
+        private static int Authenticating = 0;
+
+        public static void Authenticate(string email, string password) {
+            if (Interlocked.Exchange(ref Authenticating, 1) == 0) {
+                Task.Run(() => {
+                    Debug.WriteLine("Starting Authenticate saga");
+                    AuthResponse response = Api.Authenticate(email, password).Result;
+                    if (response.IsSuccessStatusCode()) {
+                        AppState.ReceiveUser(response.User);
+                    } else {
+                        // TODO: Emit event to show an error on the login control?
+                    }
+                    Debug.WriteLine("Ended Authenticate saga");
+                    Interlocked.Exchange(ref Authenticating, 0);
+                });
+            }
+        }
+
+    }
+}
--- OnlineOrderPrinter/State/AppState.cs
+++ OnlineOrderPrinter/State/AppState.cs
@@ -7,12 +7,18 @@ using System.Threading.Tasks;
 
 namespace OnlineOrderPrinter.State {
     class AppState {
-        public static List<Event> Events = new List<Event>();
+        public static List<Event> Events { get; set; } = new List<Event>();
+
+        public static User User { get; set; }
 
         public static void ReceiveEvents(Event[] events) {
             foreach (Event @event in events) {
                 Events.Add(@event);
             }
+        }
+
+        public static void ReceiveUser(User user) {
+            User = user;
         }
     }
 }
