b8153e1ead04a0fe86d773074c02825872b875aa
Derek Chiong <dtchiong@gmail.com>
2020-04-10 14:59:52 -0700

    Move user controls to a UserControls folder and organize by page

--- OnlineOrderPrinter/UserControls/Login/UserControlLoginPage.cs
+++ OnlineOrderPrinter/UserControls/Login/UserControlLoginPage.cs
@@ -7,9 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using Timer = System.Windows.Forms.Timer;
+
 
 namespace OnlineOrderPrinter {
     public partial class UserControlLoginPage : UserControl {
+
         public UserControlLoginPage() {
             InitializeComponent();
         }
