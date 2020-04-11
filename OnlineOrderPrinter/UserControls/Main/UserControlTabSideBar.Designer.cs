a6bca75cb12af45d52d7fa6a7a3fdef076060b9f
Derek Chiong <dtchiong@gmail.com>
2020-04-10 17:34:50 -0700

    Add restaurant label and name to design

--- OnlineOrderPrinter/UserControls/Main/UserControlTabSideBar.Designer.cs
+++ OnlineOrderPrinter/UserControls/Main/UserControlTabSideBar.Designer.cs
@@ -24,8 +24,16 @@
         /// </summary>
         private void InitializeComponent() {
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.labelRestaurantText = new System.Windows.Forms.Label();
+            this.labelRestaurantName = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
             this.SuspendLayout();
             // 
             // splitContainer1
@@ -36,10 +44,59 @@
             this.splitContainer1.Location = new System.Drawing.Point(0, 0);
             this.splitContainer1.Name = "splitContainer1";
             this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
             this.splitContainer1.Size = new System.Drawing.Size(214, 711);
             this.splitContainer1.SplitterDistance = 116;
             this.splitContainer1.SplitterWidth = 1;
             this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.BackColor = System.Drawing.Color.Transparent;
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.IsSplitterFixed = true;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.labelRestaurantText);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.labelRestaurantName);
+            this.splitContainer2.Size = new System.Drawing.Size(214, 116);
+            this.splitContainer2.SplitterDistance = 60;
+            this.splitContainer2.SplitterWidth = 1;
+            this.splitContainer2.TabIndex = 0;
+            this.splitContainer2.TabStop = false;
+            // 
+            // labelRestaurantText
+            // 
+            this.labelRestaurantText.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelRestaurantText.AutoSize = true;
+            this.labelRestaurantText.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRestaurantText.ForeColor = System.Drawing.Color.White;
+            this.labelRestaurantText.Location = new System.Drawing.Point(3, 39);
+            this.labelRestaurantText.Name = "labelRestaurantText";
+            this.labelRestaurantText.Size = new System.Drawing.Size(88, 21);
+            this.labelRestaurantText.TabIndex = 0;
+            this.labelRestaurantText.Text = "Restaurant";
+            // 
+            // labelRestaurantName
+            // 
+            this.labelRestaurantName.AutoSize = true;
+            this.labelRestaurantName.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelRestaurantName.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRestaurantName.ForeColor = System.Drawing.Color.White;
+            this.labelRestaurantName.Location = new System.Drawing.Point(0, 0);
+            this.labelRestaurantName.Name = "labelRestaurantName";
+            this.labelRestaurantName.Size = new System.Drawing.Size(0, 37);
+            this.labelRestaurantName.TabIndex = 1;
             // 
             // UserControlTabSideBar
             // 
@@ -49,8 +106,15 @@
             this.Controls.Add(this.splitContainer1);
             this.Name = "UserControlTabSideBar";
             this.Size = new System.Drawing.Size(214, 711);
+            this.splitContainer1.Panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -58,5 +122,8 @@
         #endregion
 
         private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Label labelRestaurantText;
+        private System.Windows.Forms.Label labelRestaurantName;
     }
 }
