7d2c93a3ea64fd4cebb6a03bdd07ef780f911412
Derek Chiong <dtchiong@gmail.com>
2020-04-13 21:06:38 -0700

    Remove unused UI properties from Event since we're using the better solution for formatting using the dgv's cell formatting event

--- OnlineOrderPrinter/Models/Event.cs
+++ OnlineOrderPrinter/Models/Event.cs
@@ -11,30 +11,5 @@ namespace OnlineOrderPrinter.Models {
         public DateTime CreatedAt { get; set; }
         public string RestaurantId { get; set; }
         public Order Order { get; set; }
-
-        public string UI_TimeReceived {
-            get { return CreatedAt.ToLocalTime().ToString(); }
-        }
-        public string UI_Type {
-            get { return EventType; }
-        }
-        public string UI_Service {
-            get { return Order.Service; }
-        }
-        public string UI_Name {
-            get { return Order.CustomerName; }
-        }
-        public string UI_PickupTime {
-            get { return Order.PickupTime.ToLocalTime().ToString(); }
-        }
-        public string UI_OrderSize {
-            get { return Order.OrderSize.ToString(); }
-        }
-        public string UI_ConfirmStatus {
-            get { return Order.ConfirmStatus.ToString(); }
-        }
-        public string UI_PrintStatus {
-            get { return Order.PrintStatus.ToString(); }
-        }
     }
 }
