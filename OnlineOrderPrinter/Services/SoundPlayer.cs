b66f6ce20691d7b0eb7ed55a260ed891ef05a468
Derek Chiong <dtchiong@gmail.com>
2020-05-02 13:41:13 -0700

    Add SoundPlayer service and sound assets

---
+++ OnlineOrderPrinter/Services/SoundPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineOrderPrinter.Services {
+    class SoundPlayer {
+        public static void Play(Sound sound) {
+            Stream stream = null;
+            switch (sound) {
+                case Sound.NewOrder:
+                    stream = Properties.Resources.new_order;
+                    break;
+                case Sound.CancelOrder:
+                    stream = Properties.Resources.cancel_order;
+                    break;
+            }
+            System.Media.SoundPlayer soundPlayer = new System.Media.SoundPlayer(stream);
+            soundPlayer.Play();
+        }
+    }
+
+    enum Sound {
+        NewOrder,
+        CancelOrder
+    }
+}
