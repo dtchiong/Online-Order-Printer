4b0001d30068b756e24ef4c3b729bf0ec3b1078c
Derek Chiong <dtchiong@gmail.com>
2020-05-14 23:58:24 -0700

    Add unimplemented PrinterHelper class

---
+++ OnlineOrderPrinter/Utility/PrinterHelper.cs
@@ -0,0 +1,21 @@
+﻿using OnlineOrderPrinter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineOrderPrinter.Utility {
+    class PrinterHelper {
+
+        public static List<Dictionary<int, string>> ConvertOrderToPrintableList(Order order) {
+            // TODO: Implement
+            return new List<Dictionary<int, string>>();
+        }
+
+        public static Dictionary<int, string> ConvertOrderItemToPrintableObject(OrderItem orderItem) {
+            // TODO: Implement
+            return new Dictionary<int, string>();
+        }
+    }
+}
